3153fde4578ca1489be42660d8eb5133783633ac
Sam Morris <shmorris720@gmail.com>
2021-12-09 13:14:25 -0600

    docs: preparing Cluster Manager docs for mono design

--- docs/source/docs-clustermgr-prod/update-site-local.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-git pull origin alpha
-echo -n "Enter task Performed >"
-read text
-echo "Entered Task: $text"
-git add -A
-git commit -m "updated site & - $text"
-git push origin alpha
